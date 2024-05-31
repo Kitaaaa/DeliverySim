94454194b91a1446c481bcd3385dbf0f3e1cc646
Epic Lina <sirreginaldxy@gmail.com>
2024-05-31 17:20:28 +0100

    Fiddled around with moovment

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -6,16 +6,14 @@ public class Movement : MonoBehaviour
 {
     public Rigidbody rb;
 
-    private float time = 0.0f;
-    private bool isMoving = false;
     private bool isJumpPressed = false;
-    [SerializeField] public float movementSpeed;
-    private GameObject player;
 
+    [SerializeField] public float movementSpeed;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
 
@@ -28,27 +26,18 @@ public class Movement : MonoBehaviour
 
         Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput) * movementSpeed * Time.deltaTime;
         transform.Translate(movement);
+
     }
 
     void FixedUpdate()
     {
         if (isJumpPressed)
         {
-            // the cube is going to move upwards in 10 units per second
+
+            // the cube is going to move upwards in 2 units per second
             rb.velocity = new Vector3(0, 2, 0);
-            isMoving = true;
             Debug.Log("jump");
         }
 
-        if (isMoving)
-        {
-            // when the cube has moved for 10 seconds, report its position
-            time = time + Time.fixedDeltaTime;
-            if (time > 10.0f)
-            {
-                Debug.Log(gameObject.transform.position.y + " : " + time);
-                time = 0.0f;
-            }
-        }
     }
 }
